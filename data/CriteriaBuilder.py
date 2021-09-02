21bd284efd2abfd734363e5b24caeffef6abc7a8
Terry D. Eppler <17190650+KarmaScripter@users.noreply.github.com>
2021-09-02 09:55:08 -0500

    Update CriteriaBuilder.py

--- data/CriteriaBuilder.py
+++ data/CriteriaBuilder.py
@@ -1,16 +1,25 @@
 class CriteriaBuilder():
     '''Defines the CriteriaBuilder class'''
-    __sql = None
-    __and = None
-    __where = None
+    __sql = ''
+    __and = ''
+    __where = ''
+    __criteria = { }
 
     @property
-    def and( self ):
+    def AND( self ):
         return self.__and
 
     @property
-    def where( self ):
+    def WHERE( self ):
         return self.__where
+
+    @property
+    def criteria( self ):
+        return self.__criteria
+
+    @criteria.setter
+    def criteria( self, name_value_pairs = None ):
+        self.__criteria = name_value_pairs
 
     def __init__( self ):
         self.__and = ' AND '
