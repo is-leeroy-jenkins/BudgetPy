0d76f36199a0d8e62bc3166919492c6b4ba23a4d
Terry D. Eppler <17190650+KarmaScripter@users.noreply.github.com>
2021-09-05 07:42:01 -0500

    refactored data models and budget models into single files; removed data directory and ninja directory

---
+++ Budget.py
@@ -0,0 +1,462 @@
+import datetime
+
+class Account():
+    '''defines the Account Code class'''
+    __code = ''
+    __name = None
+    __goal = ''
+    __objective = ''
+    __npm = ''
+    __program_project = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+        else:
+            return 'NS'
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__code
+        else:
+            return 'NS'
+
+    @property
+    def goal( self ):
+        if not self.__goal == '':
+            return self.__goal
+        else:
+            return 'NS'
+
+    @property
+    def npm( self ):
+        if not self.__npm == '':
+            return self.__npm
+        else:
+            return 'NS'
+
+    @property
+    def program_project( self ):
+        if not self.__program_project == '':
+            return self.__program_project
+        else:
+            return 'NS'
+
+    def __init__( self, code, name = None ):
+        self.__code = code
+        self.__name = name
+        self.__goal = list( code )[ 0 ]
+        self.__objective = list( code )[ 1:3 ]
+        self.__npm = list( code )[ 3 ]
+        self.__program_project = list( code )[ 4:6 ]
+
+    def __str__( self ):
+        if not self.__code == '':
+            return self.__code
+
+class Activity():
+    '''Defines the Activity Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name = '' ):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class AllowanceHolder():
+    '''Defines the AllowanceHolder Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name = '' ):
+        self.__ah_code = code
+        self.__a_name = name
+
+    def __str__( self ):
+        return self.__ah_code
+
+class Appropriation():
+    '''Defines the Appropriation Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name = '' ):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class BudgetFiscalYear():
+    '''Class to describe the federal fiscal year'''
+    __fiscal_year = ''
+    __today = ''
+    __date = None
+    __year = ''
+    __start_date = ''
+    __end_date = ''
+    __expiration = ''
+    __weekends = 0
+    __workdays = 0
+    __day = ''
+    __month = ''
+    __federal_holidays = { }
+
+    def __init__( self, year ):
+        self.__year = year
+        self.__today = datetime.date
+        self.__day = self.__today.day
+        self.__month = self.__today.month
+
+    def __str__( self ):
+        return self.__fiscal_year
+
+class BudgetObjectClass():
+    '''Defines the BudgetObjectClass Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name = '' ):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class Division():
+    '''Defines the Division Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name = '' ):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class FinanceObjectClass():
+    '''Defines the FinanceObjectClass Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name = '' ):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class Fund():
+    '''Defines the Fund Class'''
+    __code = ''
+    __name = ''
+    __title = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name='' ):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class Goal():
+    '''Defines the Goal Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__(self, code, name=''):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class NationalProgram():
+    '''Defines the NationalProgram Class'''
+    __code = ''
+    __name = ''
+    __rpio = ''
+    __title = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__(self, code, name=''):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class Objective():
+    '''Defines the Objective Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__( self, code, name = '' ):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class Organization():
+    '''Defines the Organization Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__(self, code, name=''):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class ProgramArea():
+    '''defines the ProgramArea class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__(self, code, name = ''):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class ProgramProject():
+    '''Defines the ProgramProject Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__(self, code, name = ''):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class ResponsibilityCenter():
+    '''Defines the ResponsibilityCenter Class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__(self, code, name = ''):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class ResourceImplementationPlanningOffice():
+    '''defines the ResponsiblePlanningOffice class'''
+    __code = ''
+    __name = ''
+
+    @property
+    def code( self ):
+        if not self.__code == '':
+            return self.__code
+
+    @property
+    def name( self ):
+        if not self.__name == '':
+            return self.__name
+
+    def __init__(self, code, name = ''):
+        self.__code = code
+        self.__name = name
+
+    def __str__( self ):
+        return self.__code
+
+class ProgramResultsCode():
+    '''Defines the PRC class'''
+    __id = -1
+    __rpio = None
+    __bfy = None
+    __ah = None
+    __fund = None
+    __org = None
+    __account = None
+    __rc = None
+    __boc = None
+    __amount = 0
+
+    @property
+    def id( self ):
+        if self.__id is not None:
+            return self.__id
+        else:
+            return - 1
+
+    @property
+    def rpio( self ):
+        if self.__rpio is not None:
+            return self.__rpio
+
+    @property
+    def bfy( self ):
+        if self.__bfy is not None:
+            return self.__bfy
+
+    @property
+    def ah( self ):
+        if self.__ah is not None:
+            return self.__ah
+
+    def __init__( self, rpio = None, bfy = None,
+                  ah = None, fund = None, code = None,
+                  boc = None, org = None ):
+        '''Initializes the PRC class'''
+
+        self.__rpio = ResourceImplementationPlanningOffice( rpio )
+        self.__bfy = BudgetFiscalYear( bfy )
+        self.__ah_code = AllowanceHolder( ah )
+        self.__fund = Fund( fund )
+        self.__account = Account( code )
+        self.__org = Organization( org )
+        self.__boc = BudgetObjectClass( boc )
---
+++ Data.py
@@ -0,0 +1,468 @@
+import os
+import pandas as pd
+import pyodbc as access
+import sqlite3
+
+class Unit:
+    '''Defines the basic budget data unit'''
+    __name = ''
+    __value = ''
+
+    @property
+    def name( self ):
+        if self.__name != '':
+            return self.__name
+
+    @property
+    def value( self ):
+        if self.__value != '':
+            return self.__value
+
+    def __init__(self, name, value):
+        self.__name = name
+        self.__value = value
+
+    def __str__(self):
+        return self.__name
+
+class BudgetFile():
+    '''Defines the BudgetFile Class'''
+    __base = None
+    __name = None
+    __path = None
+    __size = None
+    __extension = None
+    __parent_folder = None
+    __drive = None
+    __created = None
+    __modified = None
+    __accessed = None
+    __current = None
+
+    @property
+    def base( self ):
+        return self.__base
+
+    @property
+    def name( self ):
+        return os.path.basename( self.__base )
+
+    @property
+    def path( self ):
+        if self.__path is not None:
+            return self.__path
+
+    @property
+    def size( self ):
+        if self.__parent_folder is not None:
+            return self.__size
+
+    @property
+    def extension( self ):
+        if self.__extension is not None:
+            return self.__extension
+
+    @property
+    def parent_folder( self ):
+        if self.__parent_folder is not None:
+            return self.__parent_folder
+
+    @property
+    def drive( self ):
+        if self.__drive is not None:
+            return self.__drive
+
+    @property
+    def modified( self ):
+        if self.__modified is not None:
+            return self.__modified
+
+    @property
+    def accessed( self ):
+        if self.__accessed is not None:
+            return self.__accessed
+
+    @property
+    def created( self ):
+        if self.__created is not None:
+            return self.__created
+
+    # Constructor
+    def __init__( self, base ):
+        self.__base = base
+        self.__name = os.path.basename( base )
+        self.__path = os.path.abspath( base )
+        self.__size = os.path.getsize( base )
+        self.__extension = list( os.path.splitext( base ) )[ 1 ]
+        self.__created = os.path.getctime( base )
+        self.__accessed = os.path.getatime( base )
+        self.__modified = os.path.getmtime( base )
+        self.__parent_folder = os.path.dirname( base )
+
+    def get_current_directory( self ):
+        '''gets the current directory'''
+        self.__current = os.getcwd()
+        return self.__current
+
+    def rename( self, new_name ):
+        '''renames current file'''
+        return os.rename( self.__name, new_name )
+
+    def move( self, destination_path ):
+        '''renames current file'''
+        return os.path.join( self.__name, destination_path )
+
+    def exists( self ):
+        '''determines if the base file exists'''
+        if os.path.isfile( self.__base ):
+            return True
+
+    def get_drive( self ):
+        '''get the file's drive'''
+        return list(os.path.splitdrive( self.__path ))[0]
+
+    def verify( self, other_filepath ):
+        '''determines if an external file exists'''
+        if other_filepath != '':
+            return os.path.exists( other_filepath )
+
+    def get_size( self ):
+        '''gets the size of the base file'''
+        return os.path.getsize( self.__name )
+
+class BudgetFolder():
+    '''Defines the BudgetFolder Class'''
+    # pseudo-private backing fields
+    __base = None
+    __name = None
+    __path = None
+    __size = None
+    __parent_folder = None
+    __drive = None
+    __created = None
+    __modified = None
+    __accessed = None
+    __current = None
+
+    @property
+    def base( self ):
+        return self.__base
+
+    @property
+    def name( self ):
+        return os.path.basename( self.__base )
+
+    @property
+    def path( self ):
+        if self.__path is not None:
+            return self.__path
+
+    @property
+    def size( self ):
+        if self.__parent_folder is not None:
+            return self.__size
+
+    @property
+    def parent_folder( self ):
+        if self.__parent_folder is not None:
+            return self.__parent_folder
+
+    @property
+    def drive( self ):
+        if self.__drive is not None:
+            return self.__drive
+
+    @property
+    def modified( self ):
+        if self.__modified is not None:
+            return self.__modified
+
+    @property
+    def accessed( self ):
+        if self.__accessed is not None:
+            return self.__accessed
+
+    @property
+    def created( self ):
+        if self.__created is not None:
+            return self.__created
+
+    # Constructor
+    def __init__( self, base ):
+        self.__base = base
+        self.__name = os.path.basename( base )
+        self.__path = os.path.abspath( base )
+        self.__size = os.path.getsize( base )
+        self.__extension = list( os.path.splitext( base ) )[ 1 ]
+        self.__created = os.path.getctime( base )
+        self.__accessed = os.path.getatime( base )
+        self.__modified = os.path.getmtime( base )
+        self.__parent_folder = os.path.dirname( base )
+
+    def get_current_directory( self ):
+        '''gets the current directory'''
+        self.__current = os.getcwd()
+        return self.__current
+
+    def rename( self, new_name ):
+        '''renames current file'''
+        return os.rename( self.__name, new_name )
+
+    def move( self, destination_path ):
+        '''renames current file'''
+        return os.path.join( self.__name, destination_path )
+
+    def exists( self ):
+        '''determines if the base file exists'''
+        if os.path.isdir( self.__base ):
+            return True
+
+    def get_drive( self ):
+        '''get the file's drive'''
+        return list(os.path.splitdrive( self.__path ))[0]
+
+    def verify( self, other_path ):
+        '''determines if an external file exists'''
+        if other_path != '':
+            return os.path.isdir( other_path )
+
+    def get_size( self ):
+        '''gets the size of the base file'''
+        return os.path.getsize( self.__name )
+
+class CriteriaBuilder():
+    '''Defines the CriteriaBuilder class'''
+    __sql = ''
+    __and = ''
+    __where = ''
+    __criteria = { }
+
+    @property
+    def AND( self ):
+        return self.__and
+
+    @property
+    def WHERE( self ):
+        return self.__where
+
+    @property
+    def criteria( self ):
+        return self.__criteria
+
+    @criteria.setter
+    def criteria( self, name_value_pairs = None ):
+        self.__criteria = name_value_pairs
+
+    def __init__( self ):
+        self.__and = ' AND '
+        self.__where = ' WHERE '
+
+class DataRow():
+    '''Defines the DataRow Class'''
+    __items = { }
+    __value = None
+    __values = []
+    __columns = pd.Series
+    __id = -1
+
+    @property
+    def items( self ):
+        return self.__items
+
+    @property
+    def columns( self ):
+        return self.__columns
+
+    @property
+    def values( self ):
+        return self.__values
+
+    @property
+    def id( self ):
+        return self.__id
+
+    @property
+    def value( self ):
+        return self.__value
+
+    def __init__( self, items ):
+        self.__items = dict( items )
+        self.__columns = dict.fromkeys( self.__items )
+        self.__values = list( self.__items.values() )
+        self.__id = self.__values[ 0 ]
+        self.__value = self.__items.setdefault( 'Amount', 0 )
+
+    def __str__( self ):
+        return self.__id + ' ' + self.__value
+
+class DataColumn():
+    '''Defines the DataColumn Class'''
+    __name = None
+    __type = None
+    __caption = None
+    __ordinal = -1
+    __table = None
+    __row = None
+    __source = None
+    __data = { }
+
+    @property
+    def name( self ):
+        return self.__name
+
+    @property
+    def type( self ):
+        return self.__type
+
+    @property
+    def caption( self ):
+        return self.__caption
+
+    @property
+    def ordinal( self ):
+        return self.__ordinal
+
+    @property
+    def table( self ):
+        return self.__table
+
+    @property
+    def row( self ):
+        return self.__row
+
+    @property
+    def source( self ):
+        return self.__source
+
+    @property
+    def data( self ):
+        return self.__data
+
+    @property
+    def is_numeric( self ):
+        if not self.__type ==  type(''):
+            return True
+
+    @property
+    def is_text( self ):
+        if self.__type ==  type(''):
+            return True
+
+    def __init__( self, name, data_type = None,
+                  source = None, caption = None):
+        self.__name = name
+        self.__type = data_type
+        self.__caption = caption
+        self.__data = { 'name': self.__name, 'type': self.__type, 'caption': self.__caption }
+        self.__source = source
+        self.__table = self.__source
+
+    def __str__(self):
+        return self.__name
+
+class DataTable():
+    '''Defines the DataTable Class'''
+    __name = None
+    __data = { }
+    __rows = pd.DataFrame
+    __columns = pd.Series
+    __schema = { }
+    __query = None
+
+    @property
+    def name( self ):
+        return self.__name
+
+    @property
+    def data( self ):
+        return self.__data
+
+    def __init__( self, name, sql = ''  ):
+        self.__name = name
+        self.__query = sql
+
+    def __str__(self):
+        return self.__name
+
+    def get_sql( self ):
+        return self.__query
+
+    def get_source( self ):
+        return self.__name
+
+    def get_datarows( self ):
+        return self.__rows
+
+    def get_datacolumns( self ):
+        return self.__columns
+
+    def get_schema( self ):
+        return self.__schema
+
+class AccessDataBuilder():
+    '''Builds the budget execution data classes'''
+    __connector = ''
+    __connection = None
+    __cursor = None
+    __data = None
+
+    def __init__( self ):
+        self.__connector = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=accdb\Data.accdb;')
+        self.__connection = access.connect( self.__connector,
+            timeout = 3, attrs_before = dict() )
+        self.__cursor = self.__connection.cursor()
+        self.__data = ''
+
+    def get_data( self, table ):
+        if self.__data == '':
+            self.__data = self.__cursor.execute( f'SELECT * FROM {table}' )
+
+class AccessReferenceBuilder():
+    '''Builds the budget execution data classes'''
+    __connector = ''
+    __connection = None
+    __cursor = None
+    __data = None
+
+    def __init__( self ):
+        self.__connector = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=accdb\References.accdb;')
+        self.__connection = access.connect( self.__connector,
+            timeout = 3, attrs_before = dict() )
+        self.__cursor = self.__connection.cursor()
+        self.__data = ''
+
+    def get_data( self, table ):
+        if self.__data == '':
+            self.__data = self.__cursor.execute( f'SELECT * FROM {0}', table )
+
+class SQLiteDataBuilder():
+    '''Builds the budget execution data classes'''
+    __connection = None
+    __cursor = None
+    __data = None
+
+    def __init__( self ):
+        self.__connection = sqlite3.connect( 'Data.db' )
+        self.__cursor = self.__connection.cursor()
+        self.__data = ''
+
+    def get_data( self, table ):
+        if self.__data == '':
+            self.__data = self.__cursor.execute( f'SELECT * FROM {0}', table )
+
+class SQLiteReferenceBuilder():
+    '''Builds the budget execution reference models'''
+    __connection = None
+    __cursor = None
+    __data = None
+
+    def __init__( self ):
+        self.__connection = sqlite3.connect( 'References.db' )
+        self.__cursor = self.__connection.cursor()
+        self.__data = ''
+
+    def get_data( self, table ):
+        if self.__data == '':
+            self.__data = self.__cursor.execute( f'SELECT * FROM {table}' )
