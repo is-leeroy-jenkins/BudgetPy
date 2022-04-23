a63f012e87a5207f3aef3bce3373afafbdf8bc5d
KarmaScripter <terry.eppler@gmail.com>
2022-04-23 15:33:22 -0400

    updates from main rig;

--- Booger.py
+++ Booger.py
@@ -1,15 +1,20 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
 
 class BudgetWindow( QWidget ):
     '''Basic Window Class'''
     def __init__( self ):
         super( ).__init__( )
+        __black = '#050505'
+        __blue = '#2041F1'
 
         self.initializeUI( )
 
     def initializeUI( self ):
-        self.setGeometry( 100, 100, 400, 300 )
+        self.setGeometry( 100, 100, 1400, 800 )
         self.setWindowTitle( 'Budget Execution' )
         self.show( )
 
