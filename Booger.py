6d7ebcc7042966c9a473951da0bfad4c0df91b16
KarmaScripter <terry.eppler@gmail.com>
2022-06-22 09:54:12 -0400

    updates from main rig;

--- Booger.py
+++ Booger.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageTk, ImageSequence
+from PIL import Image, ImageTk, ImageSequence, ImageGrab
 import PySimpleGUI as sg
 import fitz
 import sys
@@ -12,8 +12,16 @@ from googlesearch import search
 from Minion import *
 import traceback
 import textwrap
+import numpy as np
 from pandas import read_csv as CsvReader
 from pandas import read_excel as ExcelReader
+from matplotlib.backends.backend_tkagg import FigureCanvasAgg
+import matplotlib.figure
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+from matplotlib.ticker import NullFormatter
+from mpl_toolkits.axes_grid1.axes_rgb import RGBAxes
 
 
 # Error( message = '', cause = '', method = '', module = '' )
@@ -3010,16 +3018,6 @@ class ColorDialog( Sith ):
         if isinstance( value, str ) and value != '':
             self.__html = value
 
-    @property
-    def size( self ):
-        if isinstance( self.__formsize, tuple ) :
-            return self.__formsize
-
-    @size.setter
-    def size( self, value ):
-        if isinstance( value, tuple ) :
-            self.__formsize = value
-
     def __init__( self ):
         super( ).__init__()
         self.__themebackground = super( ).themebackground
@@ -3772,6 +3770,7 @@ class ColorDialog( Sith ):
             err.show( )
 
 
+
 class BudgetForm( Sith ):
     '''class defining basic dashboard for the application'''
     __themebackground = None
@@ -4520,3 +4519,202 @@ class ExcelForm( Sith ):
             exc.method = 'show( self )'
             err = ErrorDialog( exc )
             err.show( )
+
+
+
+class GraphForm( Sith ):
+    '''Provides form that reads CSV file with pandas'''
+    __themebackground = None
+    __elementbackcolor = None
+    __elementforecolor = None
+    __themetextcolor = None
+    __textbackcolor = None
+    __inputbackcolor = None
+    __inputforecolor = None
+    __buttoncolor = None
+    __icon = None
+    __formsize = None
+    __themefont = None
+
+    def __init__( self ):
+        super( ).__init__( )
+        self.__themebackground = super( ).themebackground
+        self.__themefont = super( ).themefont
+        self.__icon = super( ).iconpath
+        self.__elementbackcolor = super( ).elementbackcolor
+        self.__elementforecolor = super( ).elementforecolor
+        self.__themetextcolor = super( ).textforecolor
+        self.__textbackcolor = super( ).textbackcolor
+        self.__inputbackcolor = super( ).inputbackcolor
+        self.__inputforecolor = super( ).inputforecolor
+        self.__buttoncolor = super( ).buttoncolor
+        self.__formsize = ( 800, 600 )
+
+    def show( self ):
+        def create_axis_grid( ):
+            plt.close( 'all' )
+
+            def get_demo_image( ):
+                delta = 0.5
+                extent = (-3, 4, -4, 3)
+                x = np.arange( -3.0, 4.001, delta )
+                y = np.arange( -4.0, 3.001, delta )
+                X, Y = np.meshgrid( x, y )
+                Z1 = np.exp( -X ** 2 - Y ** 2 )
+                Z2 = np.exp( -( X - 1 ) ** 2 - ( Y - 1 ) ** 2 )
+                Z = (Z1 - Z2) * 2
+
+                return Z, extent
+
+            def get_rgb( ):
+                Z, extent = get_demo_image( )
+
+                Z[ Z < 0 ] = 0.
+                Z = Z / Z.max( )
+
+                R = Z[ :13, :13 ]
+                G = Z[ 2:, 2: ]
+                B = Z[ :13, 2: ]
+
+                return R, G, B
+
+            fig = plt.figure( 1 )
+            ax = RGBAxes( fig, [ 0.1, 0.1, 0.8, 0.8 ] )
+
+            r, g, b = get_rgb( )
+            kwargs = dict( origin = "lower", interpolation = "nearest" )
+            ax.imshow_rgb( r, g, b, **kwargs )
+
+            ax.RGB.set_xlim( 0., 9.5 )
+            ax.RGB.set_ylim( 0.9, 10.6 )
+
+            plt.draw( )
+            return plt.gcf( )
+
+        def create_figure( ):
+            fig = matplotlib.figure.Figure( figsize = ( 5, 4 ), dpi = 100 )
+            t = np.arange( 0, 3, .01 )
+            fig.add_subplot( 111 ).plot( t, 2 * np.sin( 2 * np.pi * t ) )
+            return fig
+
+        def create_subplot_3d( ):
+            fig = plt.figure( )
+            ax = fig.add_subplot( 1, 2, 1, projection = '3d' )
+            X = np.arange( -5, 5, 0.25 )
+            Y = np.arange( -5, 5, 0.25 )
+            X, Y = np.meshgrid( X, Y )
+            R = np.sqrt( X ** 2 + Y ** 2 )
+            Z = np.sin( R )
+            surf = ax.plot_surface( X, Y, Z, rstride = 1, cstride = 1, cmap = cm.jet,
+                linewidth = 0, antialiased = False )
+
+            ax.set_zlim3d( -1.01, 1.01 )
+            fig.colorbar( surf, shrink = 0.5, aspect = 5 )
+            ax = fig.add_subplot( 1, 2, 2, projection = '3d' )
+            X, Y, Z = get_test_data( 0.05 )
+            ax.plot_wireframe( X, Y, Z, rstride = 10, cstride = 10 )
+            return fig
+
+        def create_pyplot_scales( ):
+            plt.close( 'all' )
+            np.random.seed( 19680801 )
+
+            y = np.random.normal( loc = 0.5, scale = 0.4, size = 1000 )
+            y = y[ (y > 0) & (y < 1) ]
+            y.sort( )
+            x = np.arange( len( y ) )
+
+            # plot with various axes scales
+            plt.figure( 1 )
+
+            # linear
+            plt.subplot( 221 )
+            plt.plot( x, y )
+            plt.yscale( 'linear' )
+            plt.title( 'linear' )
+            plt.grid( True )
+
+            # log
+            plt.subplot( 222 )
+            plt.plot( x, y )
+            plt.yscale( 'log' )
+            plt.title( 'log' )
+            plt.grid( True )
+
+            # symmetric log
+            plt.subplot( 223 )
+            plt.plot( x, y - y.mean( ) )
+            plt.yscale( 'symlog', linthreshy = 0.01 )
+            plt.title( 'symlog' )
+            plt.grid( True )
+
+            # logit
+            plt.subplot( 224 )
+            plt.plot( x, y )
+            plt.yscale( 'logit' )
+            plt.title( 'logit' )
+            plt.grid( True )
+            plt.gca( ).yaxis.set_minor_formatter( NullFormatter( ) )
+            plt.subplots_adjust( top = 0.92, bottom = 0.08, left = 0.10, right = 0.95,
+                hspace = 0.25,
+                wspace = 0.35 )
+
+            return plt.gcf( )
+
+        def draw_figure( element, figure ):
+            plt.close( 'all' )
+            canv = FigureCanvasAgg( figure )
+            buf = io.BytesIO( )
+            canv.print_figure( buf, format = 'png' )
+            if buf is None:
+                return None
+            buf.seek( 0 )
+            element.update( data = buf.read( ) )
+            return canv
+
+        figures = {
+                'Axis Grid':    create_axis_grid,
+                'Subplot 3D':   create_subplot_3d,
+                'Scales':       create_pyplot_scales,
+                'Basic Figure': create_figure }
+
+        def create_window( ):
+            left_col = [ [ sg.T( 'Figures to Draw' ) ],
+                         [ sg.Listbox( list( figures ),
+                             default_values = [ list( figures )[ 0 ] ], size = (15, 5),
+                             key = '-LB-' ) ],
+                         [ sg.T( 'Matplotlib Styles' ) ],
+                         [ sg.Combo( plt.style.available, size = (15, 10), key = '-STYLE-' ) ],
+                         [ sg.T( 'PySimpleGUI Themes' ) ],
+                         [ sg.Combo( sg.theme_list( ), default_value = sg.theme( ), size = (15, 10),
+                             key = '-THEME-' ) ] ]
+
+            layout = [ [ sg.T( 'Matplotlib Example', font = 'Any 20' ) ],
+                       [ sg.Col( left_col ), sg.Image( key = '-IMAGE-' ) ],
+                       [ sg.B( 'Draw' ), sg.B( 'Exit' ) ] ]
+
+            window = sg.Window( 'Matplotlib Embedded Template', layout, finalize = True )
+
+            return window
+
+        window = create_window( )
+
+        while True:
+            event, values = window.read( )
+            print( event, values )
+            if event == 'Exit' or event == sg.WIN_CLOSED:
+                break
+            if event == 'Draw':
+                if values[ '-THEME-' ] != sg.theme( ):
+                    window.close( )
+                    sg.theme( values[ '-THEME-' ] )
+                    window = create_window( )
+
+                if values[ '-LB-' ]:
+                    func = figures[ values[ '-LB-' ][ 0 ] ]
+                    if values[ '-STYLE-' ]:
+                        plt.style.use( values[ '-STYLE-' ] )
+
+                    draw_figure( window[ '-IMAGE-' ], func( ) )
+
+        window.close( )
